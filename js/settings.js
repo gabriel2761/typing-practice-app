5a20ca8b5c48957611a9a6ab5322077a7cbaca63
Gabriel Mojica <niko.mojica@live.com>
2016-06-26 02:44:26 +1000

    feat: Print range values when apply button pressed

--- js/settings.js
+++ js/settings.js
@@ -2,15 +2,22 @@ var Settings = function() {
     var $settingsView = $('#settings-view');
     var $settingsTab = $('#settings-navbar-tab');
     var $applySettingsButton = $('#apply-settings-button');
+    var $minLetterRangeInput = $('#minletter-range-input');
+    var $maxLetterRangeInput = $('#maxletter-range-input');
 
     var minLetterRange = 3;
     var maxLetterRange = 5;
+
+    $settingsTab.click(toggleVisiblity);
 
     function toggleVisiblity() {
         $settingsView.toggleClass('hidden');
     }
 
-    $settingsTab.click(toggleVisiblity);
+    function updateLetterRanges() {
+        minLetterRange = parseInt($minLetterRangeInput.val());
+        maxLetterRange = parseInt($maxLetterRangeInput.val());
+    }
 
     this.getRange = function() {
         return { 'min': minLetterRange, 'max': maxLetterRange };
@@ -22,7 +29,7 @@ var Settings = function() {
 
     this.adjustRangeListener = function(range) {
         $applySettingsButton.click(function() {
-            range({ 'min': minLetterRange, 'max': maxLetterRange });
+            updateLetterRanges();
             toggleVisiblity();
         });
     };
