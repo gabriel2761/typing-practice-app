059b67d649688233d6c5bfb546565e94d33bb4ed
Gabriel Mojica <niko.mojica@live.com>
2016-06-28 03:09:07 +1000

    feat: Create dynamic WPM function

--- js/typing-info.js
+++ js/typing-info.js
@@ -5,8 +5,21 @@ var TypingInfo = function() {
     var lettersTyped = 0;
     var wpm = 0;
 
+    var tempLettersTyped = 0;
+    var timeElapsed = 0;
+
+    setInterval(updateWpsView, 1000);
+
+    function updateWpsView() {
+        var words = tempLettersTyped / 5;
+        var wps = words / (++timeElapsed);
+        var wpm = Math.round(wps * 60);
+        $wpmView.text('WPM: ' + wpm);
+    }
+
     this.incrementTypeCount = function() {
         $lettersTypedView.text('Letters Typed ' + (++lettersTyped));
+        tempLettersTyped++;
     };
 
     this.decrementTypeCount = function() {
