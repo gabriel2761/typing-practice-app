48c6c2e6a454539fd61dc3fae8e72e5e30bc85ba
Gabriel Mojica <niko.mojica@live.com>
2016-06-28 03:49:59 +1000

    fix: Reset wpm if user stops typing

--- js/typing-info.js
+++ js/typing-info.js
@@ -1,25 +1,37 @@
 var TypingInfo = function() {
     var $lettersTypedView = $('#letters-typed-view');
     var $wpmView = $('#wpm-view');
-
     var lettersTyped = 0;
     var wpm = 0;
-
     var tempLettersTyped = 0;
     var timeElapsed = 0;
+    var afkTimeout = null;
 
     setInterval(updateWpsView, 1000);
+
+    function setAfkTimeout() {
+        afkTimeout = setTimeout(function() {
+            timeElapsed = 0;
+            tempLettersTyped = 0;
+            console.log('timer out cleared');
+        }, 7000);
+    }
+
+    function clearAfkTimeout() {
+        clearTimeout(afkTimeout);
+    }
 
     function updateWpsView() {
         var words = tempLettersTyped / 5;
         var wps = words / (++timeElapsed);
-        var wpm = Math.round(wps * 60);
-        $wpmView.text('WPM: ' + wpm);
+        $wpmView.text('WPM: ' + Math.round(wps * 60));
     }
 
     this.incrementTypeCount = function() {
         $lettersTypedView.text('Letters Typed ' + (++lettersTyped));
         tempLettersTyped++;
+        clearAfkTimeout();
+        setAfkTimeout();
     };
 
     this.decrementTypeCount = function() {
