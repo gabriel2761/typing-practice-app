b618660265266294e59793b96857487f757d85e7
Gabriel Mojica <niko.mojica@live.com>
2016-06-28 04:16:32 +1000

    feat: Stop wps from updating if letters are not typed

--- js/typing-info.js
+++ js/typing-info.js
@@ -2,29 +2,37 @@ var TypingInfo = function() {
     var $lettersTypedView = $('#letters-typed-view');
     var $wpmView = $('#wpm-view');
     var lettersTyped = 0;
-    var wpm = 0;
     var tempLettersTyped = 0;
     var timeElapsed = 0;
     var afkTimeout = null;
-
-    setInterval(updateWpsView, 1000);
+    var wpsIntervalUpdate = null;
 
     function setAfkTimeout() {
         afkTimeout = setTimeout(function() {
             timeElapsed = 0;
             tempLettersTyped = 0;
+            stopWpsIntervalUpdate();
             console.log('timer out cleared');
         }, 7000);
     }
 
-    function clearAfkTimeout() {
-        clearTimeout(afkTimeout);
+    function setWpsIntervalUpdate() {
+        if (wpsIntervalUpdate !== null) return;
+        wpsIntervalUpdate = setInterval(function() {
+            var words = tempLettersTyped / 5;
+            var wps = words / (++timeElapsed);
+            $wpmView.text('WPM: ' + Math.round(wps * 60));
+            console.log(tempLettersTyped + ' ' + timeElapsed);
+        }, 1000);
     }
 
-    function updateWpsView() {
-        var words = tempLettersTyped / 5;
-        var wps = words / (++timeElapsed);
-        $wpmView.text('WPM: ' + Math.round(wps * 60));
+    function stopWpsIntervalUpdate() {
+        clearInterval(wpsIntervalUpdate);
+        wpsIntervalUpdate = null;
+    }
+
+    function clearAfkTimeout() {
+        clearTimeout(afkTimeout);
     }
 
     this.incrementTypeCount = function() {
@@ -32,6 +40,7 @@ var TypingInfo = function() {
         tempLettersTyped++;
         clearAfkTimeout();
         setAfkTimeout();
+        setWpsIntervalUpdate();
     };
 
     this.decrementTypeCount = function() {
